565648cf5d12bb2d912bc5f90ad2c335e3a1903d
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-04-28 16:42:58 +0200

    Implementing the Model class and a placeholder main_new.py.

--- classes/model.py
+++ classes/model.py
@@ -1,8 +1,20 @@
-from torch import nn
+from torch import nn, Tensor
 
-class model(nn.Module):
+class Model(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
+        self.linear_stack = nn.Sequential(
+            nn.Linear(2, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 1),
+            nn.Sigmoid()
 
-    def forward(self):
-        pass
+        )
+
+    def forward(self, x_input_data: Tensor):
+        a_activation = self.linear_stack(x_input_data)
+        return a_activation 
