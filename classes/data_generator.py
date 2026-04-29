7ee889e83918b5147eb7028408c83dd60e2b4a66
ThoHuj <nyheterna-vid.0v@icloud.com>
2026-04-30 01:08:25 +0200

    Starting to implement torchvision.

--- classes/data_generator.py
+++ classes/data_generator.py
@@ -1,12 +1,26 @@
-from torch import Tensor, mean, rand
-from torch.types import Device
+from torch import Tensor
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms  # type: ignore
 
 
 class DataGenerator:
-    def generate_random_image_data(
-        self, batch_size: int, device: Device
-    ) -> tuple[Tensor, Tensor]:
-        random_image_data_vector = rand(batch_size, 2, device=device)
-        column_means = mean(random_image_data_vector, dim=1)
-        y_true_label_vector = (column_means < 0.5).float().reshape(-1, 1)
-        return random_image_data_vector, y_true_label_vector
+    def __init__(self, batch_size: int = 64):
+        self.batch_size = batch_size
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+        train_dataset = datasets.MNIST(
+            root="./data", train=True, download=True, transform=transform
+        )
+        test_dataset = datasets.MNIST(
+            root="./data", train=False, download=True, transform=transform
+        )
+        self.train_loader: DataLoader[tuple[Tensor, Tensor]] = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=True
+        )
+        self.test_loader: DataLoader[tuple[Tensor, Tensor]] = DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=False
+        )
